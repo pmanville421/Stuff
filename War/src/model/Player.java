42f12626a01c1f29ecb66be2e82a1dd52cac6a63
pmanville <pmanville@WR-C102A-03A.centralgatech.edu>
2016-03-10 13:35:27 -0500

    player

--- War/src/model/Player.java
+++ War/src/model/Player.java
@@ -1,6 +1,41 @@
 package model;
 
-public class Player {
+import java.io.Serializable;
 
+public class Player implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private Hand hand;
+
+	/**
+	 * @param name
+	 * @param hand
+	 */
+	public Player(String name, Hand hand) {
+		super();
+		this.name = name;
+		this.hand = hand;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Hand getHand() {
+		return hand;
+	}
+
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+	
 	
 }
