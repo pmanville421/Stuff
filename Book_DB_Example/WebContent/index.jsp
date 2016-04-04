d5c83bb778c48f72fe538cf5918d05139bc58fac
Phillip <plm12868@uga.edu>
2016-04-03 23:32:16 -0400

    Example

---
+++ Book_DB_Example/WebContent/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Amazing Book Database Demonstration</title>
+</head>
+<body>
+<h1>Amazing Book Database Demonstration</h1>
+
+<p>
+	<a href="read">Read the Database</a>
+</p>
+
+</body>
+</html>
---
+++ Book_DB_Example/src/model/Book.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author plm12
+ *
+ */
+public class Book {
+
+		private int bookID;
+		private String title;
+		private String author;
+		private int pages;
+		
+		/**
+		 * @param bookID
+		 * @param title
+		 * @param author
+		 * @param pages
+		 */
+		public Book() {
+			this.bookID = 1;
+			this.title = "No Title";
+			this.author = "No Author";
+			this.pages = 0;
+		}
+		public Book(int bookID, String title, String author, int pages) {
+			this.bookID = bookID;
+			this.title = title;
+			this.author = author;
+			this.pages = pages;	
+		}
+		/**
+		 * @return the bookID
+		 */
+		public int getBookID() {
+			return bookID;
+		}
+		/**
+		 * @param bookID the bookID to set
+		 */
+		public void setBookID(int bookID) {
+			this.bookID = bookID;
+		}
+		/**
+		 * @return the title
+		 */
+		public String getTitle() {
+			return title;
+		}
+		/**
+		 * @param title the title to set
+		 */
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		/**
+		 * @return the author
+		 */
+		public String getAuthor() {
+			return author;
+		}
+		/**
+		 * @param author the author to set
+		 */
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+		/**
+		 * @return the pages
+		 */
+		public int getPages() {
+			return pages;
+		}
+		/**
+		 * @param pages the pages to set
+		 */
+		public void setPages(int pages) {
+			this.pages = pages;
+		}
+		
+		
+}
